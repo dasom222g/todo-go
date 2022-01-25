98a7dff9fae9af0286961f537cf9a51d2b3e9d96
dasom <dasom228@gmail.com>
2022-01-26 00:19:49 +0900

    Add frontend (delete)

--- public/todo.js
+++ public/todo.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('.form').querySelector('form')
 const listArea = document.querySelector('.todo__list')
-
 const header = {
   headers: {
     'Content-Type': 'application/json',
@@ -11,9 +10,8 @@ const header = {
 const getTodos = async () => {
   const response = await fetch('/todos', header)
   const result = await response.json()
-  console.log('response', response, 'result', result)
   result.forEach((todo) => {
-    addItem(todo)
+    setTodo(todo)
   })
 }
 
@@ -35,17 +33,34 @@ const handleSubmit = async (e) => {
   })
   const result = await response.json()
   console.log('result', result)
-  addItem(result)
+  setTodo(result)
   reset(input)
 }
 
+const removeTodo = async (id, element) => {
+  const response = await fetch(`todos/${id}`, { method: 'DELETE' })
+  const result = await response.json()
+  result.success && element.remove()
+  console.log('element', element)
+}
+
+const handleClick = (e) => {
+  target = e.target
+  switch (target.dataset.eventType) {
+    case 'delete':
+      element = target.closest('li')
+      removeTodo(element.dataset.id, element)
+      break
+  }
+}
+
 // ui
-const addItem = (item) => {
+const setTodo = (item) => {
   console.log('item', item)
   const li = document.createElement('li')
   li.className = `todo__item`
   li.setAttribute('data-id', item.id)
-  const html = `<div class="todo__content ${item.is_complete ? 'complete' : ''}"><div class="todo__item-check"><label><input type="checkbox"><i class="fas fa-square todo__item-check-icon"></i><i class="fas fa-check-square todo__item-check-icon complete"></i><span class="todo__content-text">${item.title}</span></label></div><div class="todo__item-buttonarea"><button type="button" class="todo__item-button"><i class="fas fa-trash-alt"></i></button></div></div>`
+  const html = `<div class="todo__content ${item.is_complete ? 'complete' : ''}"><div class="todo__item-check"><label><input type="checkbox"><i class="fas fa-square todo__item-check-icon"></i><i class="fas fa-check-square todo__item-check-icon complete"></i><span class="todo__content-text">${item.title}</span></label></div><div class="todo__item-buttonarea"><button type="button" class="todo__item-button"><i class="fas fa-trash-alt" data-event-type="delete"></i></button></div></div>`
   li.innerHTML = html
   item.is_complete && li.querySelector('input').setAttribute('checked', item.is_complete)
   listArea.appendChild(li)
@@ -58,4 +73,5 @@ const reset = (input) => {
 
 getTodos()
 
+listArea.addEventListener('click', handleClick)
 form.addEventListener('submit', handleSubmit)
