60ececd29769948fce9120311b642ee35dac6125
dasom <dasom228@gmail.com>
2022-01-26 23:39:47 +0900

    Add complete todo

--- public/todo.js
+++ public/todo.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.form').querySelector('form')
 const listArea = document.querySelector('.todo__list')
+// const completeCheckbox = listArea.querySelector(`input[type='checkbox']`)
 const header = {
   headers: {
     'Content-Type': 'application/json',
@@ -42,12 +43,26 @@ const removeTodo = async (id, element) => {
   result.success && element.remove()
 }
 
+const changeCompleteTodo = async (id, checked, element) => {
+  // 'todos/1?complete=true'
+  const content = element.querySelector('.todo__content')
+  const response = await fetch(`todos/${id}?complete=${checked}`)
+  const result = await response.json()
+  content.classList.remove('complete') // 초기화
+  console.log('result', result)
+  const className = checked ? 'complete' : '' 
+  content.classList.add(className)
+}
+
 const handleClick = (e) => {
   target = e.target
+  const element = target.closest('li')
   switch (target.dataset.eventType) {
     case 'delete':
-      element = target.closest('li')
       removeTodo(element.dataset.id, element)
+      break
+    case 'changeComplete':
+      changeCompleteTodo(element.dataset.id, target.checked, element)
       break
   }
 }
@@ -57,7 +72,7 @@ const setTodo = (item) => {
   const li = document.createElement('li')
   li.className = `todo__item`
   li.setAttribute('data-id', item.id)
-  const html = `<div class="todo__content ${item.is_complete ? 'complete' : ''}"><div class="todo__item-check"><label><input type="checkbox"><i class="fas fa-square todo__item-check-icon"></i><i class="fas fa-check-square todo__item-check-icon complete"></i><span class="todo__content-text">${item.title}</span></label></div><div class="todo__item-buttonarea"><button type="button" class="todo__item-button"><i class="fas fa-trash-alt" data-event-type="delete"></i></button></div></div>`
+  const html = `<div class="todo__content ${item.is_complete ? 'complete' : ''}"><div class="todo__item-check"><label><input type="checkbox" data-event-type="changeComplete"><i class="fas fa-square todo__item-check-icon"></i><i class="fas fa-check-square todo__item-check-icon complete"></i><span class="todo__content-text">${item.title}</span></label></div><div class="todo__item-buttonarea"><button type="button" class="todo__item-button"><i class="fas fa-trash-alt" data-event-type="delete"></i></button></div></div>`
   li.innerHTML = html
   item.is_complete && li.querySelector('input').setAttribute('checked', item.is_complete)
   listArea.appendChild(li)
